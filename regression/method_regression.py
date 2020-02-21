47b0e2f33bfd1f316a2ada279709a6cafa8981d2
TrungLuong1194 <trungluong1194@gmail.com>
2020-02-21 12:00:19 +0300

    update

--- regression/method_regression.py
+++ regression/method_regression.py
@@ -48,6 +48,15 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # ---------------------------------------------------------------------------------
+# Feature Scaling
+# ---------------------------------------------------------------------------------
+from sklearn.preprocessing import StandardScaler
+
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+# ---------------------------------------------------------------------------------
 # Decision Tree Regression
 # Fitting the Decision Tree Regression Model to the dataset
 # ---------------------------------------------------------------------------------
